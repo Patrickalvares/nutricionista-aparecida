8b59a106cf37e0e9cdeca4768e5942409668c2a7
Patrick Alvares <patrickalvares@outlook.com>
2022-08-14 21:41:18 -0300

    creation of the loop to calculate imc of all patients and styling of the page.

--- js/principal.js
+++ js/principal.js
@@ -1,20 +1,21 @@
-var titulo = document.querySelector(".titulo");
-titulo.textContent = "Aparecida Nutricionista";
+var pacientes = document.querySelectorAll(".paciente");
 
-var paciente = document.querySelector("#paciente1");
+for (var i = 0; i < pacientes.length; i++) {
+	var paciente = pacientes[i];
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+	var tdPeso = paciente.querySelector(".info-peso");
+	var peso = tdPeso.textContent;
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+	var tdAltura = paciente.querySelector(".info-altura");
+	var altura = tdAltura.textContent;
 
-var tdImc = paciente.querySelector(".info-imc");
+	var tdImc = paciente.querySelector(".info-imc");
 
-if (peso > 10 && peso < 400 && altura > 0.20 && altura < 3.00) {
-	var imcNovo = peso / (altura * altura);
-	tdImc.textContent = imcNovo;
-}
-else {
-	tdImc.textContent = "Peso ou altura invalida";
+	if (peso > 10 && peso < 400 && altura > 0.20 && altura < 3.00) {
+		var imcNovo = peso / (altura * altura);
+		tdImc.textContent = imcNovo.toFixed(2);
+	}
+	else {
+		tdImc.textContent = "Peso ou altura invalida";
+	}
 }
